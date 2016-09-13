8bf567c56fdaf68546a7df0de74c1bb2bab94549
ViolaZhou7 <zypviole@hotmail.com>
2016-09-13 15:18:27 -0700

    Add files via upload

--- Algorithms/src/BubbleSort.java
+++ Algorithms/src/BubbleSort.java
@@ -1,33 +1,27 @@
 package Sorting;
 
-public class SelectionSort {
+public class BubbleSort {
 	// time complexity: O(n^2)
 	// space complexity: 1
 	public static void main(String[] args) {
-		int[] arr = {5,4,3,6,7,2,8,9,0,3,4,8,3};
+		int[] arr = {56,9,56,3,53,6,42,6,2,6,7,14,78,4,2};
 		sortArray(arr);
 		printArray(arr);
 	}
 
 	private static void sortArray(int[] arr) {
-		int index; // index of min value
 		for(int i=0;i<arr.length-1;i++){
-			index = i; // initialize the index of min as the first index in the loop
 			for(int j=i+1;j<arr.length;j++){
-				if(arr[j] < arr[index]){
-					index = j;
+				if(arr[i] > arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
-			
-			// exchange values between arr[i] and min
-			if(i != index){
-				int temp = arr[i];
-				arr[i] = arr[index];
-				arr[index] = temp;
-			}
 		}
+		
 	}
-
+	
 	private static void printArray(int[] arr) {
 		for(int i=0;i<arr.length;i++){
 			System.out.println(arr[i]);
